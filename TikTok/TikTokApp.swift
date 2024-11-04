a8646832192a9855d5844f79f5d632f5185aa7e4
vhenriquebm <vhenriquebm@gmail.com>
2024-11-04 14:16:22 -0300

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Vitor Henrique Barreiro Marinho on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
