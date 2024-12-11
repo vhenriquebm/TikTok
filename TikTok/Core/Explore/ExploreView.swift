739275ec20068b812daf7159c889408cb5d74651
vhenriquebm <vhenriquebm@gmail.com>
2024-12-11 19:15:35 -0300

    Explore view for friends list created

---
+++ TikTok/Core/Explore/ExploreView.swift
@@ -0,0 +1,33 @@
+//
+//  ExploreView.swift
+//  TikTok
+//
+//  Created by Vitor Henrique Barreiro Marinho on 11/12/24.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                
+                LazyVStack(spacing: 16) {
+                    
+                    ForEach(0..<20) { user in
+                        UserCell()
+                            .padding(.horizontal)
+                    }
+                    
+                }
+            }
+        }
+        .navigationTitle("Explore")
+        .navigationBarTitleDisplayMode(.inline)
+        .padding(.top)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
---
+++ TikTok/Core/Explore/UserCell.swift
@@ -0,0 +1,36 @@
+//
+//  UserCell.swift
+//  TikTok
+//
+//  Created by Vitor Henrique Barreiro Marinho on 11/12/24.
+//
+
+import SwiftUI
+
+struct UserCell: View {
+    
+    var body: some View {
+        
+        HStack(spacing: 12) {
+            Image(systemName: "person.circle.fill")
+                .resizable()
+                .frame(width: 48, height: 48)
+                .foregroundStyle(Color(.systemGray5))
+            
+            VStack(alignment: .leading) {
+                Text("test_user")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                
+                Text("Test Name")
+                    .font(.footnote)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    UserCell()
+}
